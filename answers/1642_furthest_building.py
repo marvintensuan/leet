2d18170ebfd1af04f6accfb77af1f0bb118a34d6
Marvin <mdtensuan_dev@protonmail.com>
2022-06-26 01:07:49 +0800

    fixed typo

--- answers/1642_furthest_building.py
+++ answers/1642_furthest_building.py
@@ -36,7 +36,7 @@ if __name__ == "__main__":
 
     CASES = (
         (furthest_building(heights=[4, 2, 7, 6, 9, 14, 12], bricks=5, ladders=1), 4),
-        (furthest_building(heights=[4, 12, 2, 7, 3, 18, 20, 3, 19], bricks=10, ladders=2), 8),
+        (furthest_building(heights=[4, 12, 2, 7, 3, 18, 20, 3, 19], bricks=10, ladders=2), 7),
         (furthest_building(heights=[14, 3, 19, 3], bricks=17, ladders=0), 3),
         (furthest_building(heights=[1, 5, 1, 2, 3, 4], bricks=4, ladders=1), 5),
     )
